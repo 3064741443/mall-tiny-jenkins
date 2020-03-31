13073879b27d879a64f0251e6d2b612c546a659b
james <James@yonglang.co>
2020-03-31 13:22:27 +0800

    修改虚拟机Ip地址为db

--- src/main/resources/mall-tiny-jenkins.sh
+++ src/main/resources/mall-tiny-jenkins.sh
@@ -5,7 +5,7 @@ echo '----stop container----'
 docker rm ${app_name}
 echo '----rm container----'
 docker run -p 8088:8088 --name ${app_name} \
---link mysql:192.168.117.161 \
+--link mysql:db \
 -v /etc/localtime:/etc/localtime \
 -v /mydata/app/${app_name}/logs:/var/logs \
 -d mall-tiny/${app_name}:1.0-SNAPSHOT
